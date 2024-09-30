09b47ef83948d2f1263d3ec5636a2562b32dd029
hey pritesh <priteshdhanad2001@gmail.com>
2024-09-30 11:30:02 +0530

    Understood abstraction but it needs revision

---
+++ PureTypescript/src/abstraction.ts
@@ -0,0 +1,10 @@
+// abstract classes in typescript :
+
+// abstract classes are declared using the 'abstract' keyword.
+// They cannot be instantiated directly, but can be inherited by other classes.
+// Abstract classes can contain abstract methods, which must be implemented in the derived classes.
+// Abstract classes can also contain non-abstract methods.
+
+// abstract classes in typescript example :
+
+// Imp : need to study this.
